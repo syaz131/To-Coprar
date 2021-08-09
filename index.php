ddb62b34d33be9c91be46a21d25b8573e433a67f
syaz131 <ewan.salleh.99@gmail.com>
2021-08-09 13:26:11 +0800

    Start file

---
+++ index.php
@@ -0,0 +1,18 @@
+
+
+<?php
+    
+    //http://localhost/export%20excel/
+
+    $a = 10;
+    $b = 20;
+
+    $result = "Have two values:";
+
+    echo "hello piee";
+?>
+
+<br/>
+ <?php echo $result." ".$a." and ". $b;?>
+
+<?php  ; ?>
